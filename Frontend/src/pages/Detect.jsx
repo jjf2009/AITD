100aa6d55759a3048f28fd9b9dd24571eff0475d
jjf2009 <jaredfurtadowork@gmail.com>
2025-04-10 16:46:21 +0530

    file routes

---
+++ Frontend/src/pages/Detect.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Detect = () => {
+  return (
+    <div>Detect</div>
+  )
+}
+
+export default Detect
---
+++ Frontend/src/routers/router.jsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App.jsx";
+import Weather from "@/pages/Weather.jsx";
+import Detect from "@/pages/Detect.jsx";
+import Manage from "@/pages/Manage.jsx"
+import Home from "@/pages/Home.jsx";
+import Login from "@/pages/Login.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/weather",
+        element: <Weather/>
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/detect",
+        element: <Detect/>
+      },
+      {
+        path: "/manage",
+        element: <Manage/>
+      },
+    ],
+  },
+]);
+
+export default router;
